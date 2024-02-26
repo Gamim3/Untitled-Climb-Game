374acfd3b6841570f98dde34a1fd5a61036d2dbf
Roel de Bruijn <roel.de.bruijn@gmail.com>
2024-02-26 11:35:25 +0100

    player changes aan klimmen
    collider en movement veranderen

---
+++ Untitled Climb Game/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    public float moveX;
+    public float moveZ;
+    public float rotationY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
