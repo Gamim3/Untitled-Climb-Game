75706a5de473804e206d9117269b2a5a6eaf9d31
Gamim3 <99258180+Gamim3@users.noreply.github.com>
2024-03-17 21:44:26 +0100

    mapupdates

---
+++ Untitled Climb Game/Assets/Scripts/Quickrestart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quickrestart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        return;
+    }
+}
