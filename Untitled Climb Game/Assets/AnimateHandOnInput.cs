ec14489b4a0cbf25795e22e37a91d185901d391c
Gamim3 <99258180+Gamim3@users.noreply.github.com>
2024-04-08 11:32:52 +0200

    map updates

--- Untitled Climb Game/Assets/AnimateHandOnInput.cs
+++ Untitled Climb Game/Assets/AnimateHandOnInput.cs
@@ -10,6 +10,7 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty gripAnimationAction;
     public InputActionProperty thumbTouch;
     public Animator handAnimator;
+    public GameObject _Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +20,23 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
+        if(_Player.GetComponent<GravityWhileClimbing>().isAttachedL != true || _Player.GetComponent<GravityWhileClimbing>().isAttachedR != true)
+        {
+            handAnimator.SetFloat("Trigger", 1f);
+            handAnimator.SetFloat("Grip", 1f);
+            handAnimator.SetFloat("TouchPadTouch", 1f);
+        }
+        else
+        {
+            float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("Trigger", triggerValue);
 
-        float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
+            float gripValue = gripAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("Grip", gripValue);
 
-        float thumbValue = thumbTouch.action.ReadValue<float>();
-        handAnimator.SetFloat("TouchPadTouch", thumbValue);
+            float thumbValue = thumbTouch.action.ReadValue<float>();
+            handAnimator.SetFloat("TouchPadTouch", thumbValue);
+        }
+
     }
 }
