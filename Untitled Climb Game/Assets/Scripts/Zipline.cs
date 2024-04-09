d89edee78ca6e278070b4d39a146f2232605d6ea
Gamim3 <99258180+Gamim3@users.noreply.github.com>
2024-04-09 16:11:34 +0200

    Update Zipline.cs

--- Untitled Climb Game/Assets/Scripts/Zipline.cs
+++ Untitled Climb Game/Assets/Scripts/Zipline.cs
@@ -41,7 +41,7 @@ public class Zipline : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        avgHandPos = player.GetComponent<DynoJump>().averageHandPosition;
+
     }
   
 
