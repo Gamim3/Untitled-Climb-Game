4a69a5b8434be317bb7091bfbaeb4b8b42b857de
Gamim3 <99258180+Gamim3@users.noreply.github.com>
2024-04-10 13:26:12 +0200

    head low lock

--- Untitled Climb Game/Assets/Scripts/ColliderFollowHead.cs
+++ Untitled Climb Game/Assets/Scripts/ColliderFollowHead.cs
@@ -19,12 +19,15 @@ public class ColliderFollowHead : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _hight = _head.position.y - _Feet.position.y;
-        float halfHight = _hight / 2;
-        gameObject.GetComponent<CapsuleCollider>().center = new Vector3(_head.localPosition.x, _Feet.localPosition.y + halfHight, _head.localPosition.z);
-        gameObject.GetComponent<CapsuleCollider>().height = _hight;
-        _followCam.transform.localPosition = new Vector3(_head.localPosition.x, _Feet.localPosition.y + halfHight, _head.localPosition.z);
-        _followCam.transform.localRotation = Quaternion.Euler(_Feet.rotation.x, _head.rotation.y, _Feet.rotation.y);
+        if(_head.localRotation.eulerAngles.x <= 40)
+        {
+            _hight = _head.position.y - _Feet.position.y;
+            float halfHight = _hight / 2;
+            gameObject.GetComponent<CapsuleCollider>().center = new Vector3(_head.localPosition.x, _Feet.localPosition.y + halfHight, _head.localPosition.z);
+            gameObject.GetComponent<CapsuleCollider>().height = _hight;
+            _followCam.transform.localPosition = new Vector3(_head.localPosition.x, _Feet.localPosition.y + halfHight, _head.localPosition.z);
+            _followCam.transform.localRotation = Quaternion.Euler(_Feet.rotation.x, _head.rotation.y, _Feet.rotation.y);
+        }
     }
 
 }
